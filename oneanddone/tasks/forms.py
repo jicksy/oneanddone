53d74a9c376786368fe46496b85f4902064386b8
jicksy <jicksy.john@gmail.com>
2014-11-01 21:09:00 +0530

    Update forms.py

--- oneanddone/tasks/forms.py
+++ oneanddone/tasks/forms.py
@@ -45,7 +45,10 @@ class PreviewConfirmationForm(forms.Form):
 
 
 class TaskForm(forms.ModelForm):
-    admin_time= forms.TextInput(attrs={'size': 100, 'class': 'fill-width'})
+    admin_time = (forms.CharField(
+                help_text=_('Enter If more than 60 minutes.'),
+                required=False,
+                widget=forms.TextInput(attrs={'class': 'fill-width'})))
     keywords = (forms.CharField(
                 help_text=_('Please use commas to separate your keywords.'),
                 required=False,
