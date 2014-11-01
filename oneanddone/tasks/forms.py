600c538b4efa68ac3a8e6aba3b6737a231177499
jicksy <jicksy.john@gmail.com>
2014-11-02 05:15:27 +0530

    Update forms.py

--- oneanddone/tasks/forms.py
+++ oneanddone/tasks/forms.py
@@ -45,7 +45,7 @@ class PreviewConfirmationForm(forms.Form):
 
 
 class TaskForm(forms.ModelForm):
-    admin_time = (forms.IntegerField(
+    admin_time = (forms.CharField(
                 help_text=_('Enter If more than 60 minutes.'),
                 required=False,
                 widget=forms.TextInput(attrs={'class': 'fill-width'})))
